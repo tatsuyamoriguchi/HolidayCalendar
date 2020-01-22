77a5d23b7ab21f78cdc79ecf3ad02bda597dce87
Brian Moriguchi <brian@beckos.com>
2020-01-22 08:27:00 -0800

    Initial Commit

---
+++ HolidayCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolidayCalendar
+//
+//  Created by Tatsuya Moriguchi on 1/22/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
